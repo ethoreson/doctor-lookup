a85f9e62472eb5a128952e7157bd2c5c0b582429
Emilie Thoreson <thoresonemilie@gmail.com>
2017-08-04 15:42:45 -0700

    seperate front and backend logic

--- js/symptom-interface.js
+++ js/symptom-interface.js
@@ -1,5 +1,3 @@
-var apiKey = require('./../.env').apiKey;
-
 var Symptom = require('./../js/symptom.js').symptomModule;
 
 var displayDoctors = function(symptom, doctors) {
@@ -10,14 +8,13 @@ $(document).ready(function() {
   var newSymptom = new Symptom();
   $('#symptom-form').click(function() {
     var medicalIssue = $('#retrieve-symptom').val();
+    var doctors = "Doctor A";
     $('#retrieve-symptom').val("");
-
-    $('.confirm-input').text("Input received: " + medicalIssue);
-    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=47.6062%2C-122.3321%2C%205&user_location=47.6062%2C-122.3321&skip=0&limit=3&user_key=' + apiKey).then(function(response) {
-      console.log(response);
+    newSymptom.getDoctors(medicalIssue, doctors);
+    // $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=47.6062%2C-122.3321%2C%205&user_location=47.6062%2C-122.3321&skip=0&limit=3&user_key=' + apiKey).then(function(response) {
     // newSymptom.getDoctors(symptom, displayDoctors);
     //    console.log(response);
     //  console.log(JSON.stringify(response));
-    });
+    // });
   });
 });
