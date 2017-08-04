7c2f313613d625e4b0194053974b7bc37c6cfb76
Emilie Thoreson <thoresonemilie@gmail.com>
2017-08-04 12:36:32 -0700

    fix reload index bug

--- js/symptom-interface.js
+++ js/symptom-interface.js
@@ -12,11 +12,11 @@ $(document).ready(function() {
     var medicalIssue = $('#retrieve-symptom').val();
     $('#retrieve-symptom').val("");
     $('.confirm-input').text("Input received: " + medicalIssue);
-    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=1&user_key=' + apiKey, function(response) {
+    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=47.6062%2C-122.3321%2C%205&user_location=47.6062%2C-122.3321&skip=0&limit=1&user_key=' + apiKey, function(response) {
       console.log(response);
     // newSymptom.getDoctors(symptom, displayDoctors);
     //    console.log(response);
-      console.log(JSON.stringify(response));
+    //  console.log(JSON.stringify(response));
     });
   });
 });
