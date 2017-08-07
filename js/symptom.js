cd317ba748124cc1d175b14e1257e762317979ce
Guest User <Guest@epicodus-107.local>
2017-08-07 09:44:18 -0700

    display dr.'s last name

--- js/symptom.js
+++ js/symptom.js
@@ -1,19 +1,1 @@
 var apiKey = require('./../.env').apiKey;
-
-
-// function Symptom(){
-// }
-
-Symptom = function(){
-};
-
-Symptom.prototype.getDoctors = function(medicalIssue, displayDoctors) {
-  console.log("works!");
-  $.get('https://api.betterdoctor.com/2016-03-01/specialties?fields=' + medicalIssue + '&skip=0&limit=2&user_key=' + apiKey).then(function(response) {
-    $('.displayDoctors').text("Here are the doctors you should see:");
-  }).fail(function(error) {
-    $('.displayDoctors').text('error');
-  });
-};
-
-exports.symptomModule = Symptom;
