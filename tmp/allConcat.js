19bed5e6fcd0499ba706646ed2ae680e2c3acfe4
Emilie Thoreson <thoresonemilie@gmail.com>
2017-08-07 09:52:26 -0700

    display newly formatted info, including first name and bio

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -8,7 +8,11 @@ $(document).ready(function() {
     $(".doctor-results").text("Based on your symptom of " + symptom + "...");
     $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ symptom+'&location=45.5231%2C-122.6765%2C%205&user_location=47.6062%2C-122.3321&skip=0&limit=2&user_key=' + apiKey, function(response) {
       console.log(response);
-      $('.doctor-results').text("We reccomend you contact " + response.data[0].profile.last_name);
+      $('.doctor-results').append("We reccomend you contact:<br>Dr." + response.data[0].profile.first_name +
+      " " +
+      response.data[0].profile.last_name +
+      ":</h3><br>" +
+      response.data[0].profile.bio);
     });
   });
 });
