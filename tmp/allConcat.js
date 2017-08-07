7b638a4e53951acc14de9e75ee9927896df65311
Emilie Thoreson <thoresonemilie@gmail.com>
2017-08-07 10:02:10 -0700

    display 3 results using for loop

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -6,13 +6,16 @@ $(document).ready(function() {
     var symptom = $("#retrieve-symptom").val();
     $('#retrieve-symptom').val("");
     $(".doctor-results").text("Based on your symptom of " + symptom + "...");
-    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ symptom+'&location=45.5231%2C-122.6765%2C%205&user_location=47.6062%2C-122.3321&skip=0&limit=2&user_key=' + apiKey, function(response) {
+    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ symptom+'&location=45.5231%2C-122.6765%2C%205&user_location=47.6062%2C-122.3321&skip=0&limit=3&user_key=' + apiKey, function(response) {
       console.log(response);
-      $('.doctor-results').append("We reccomend you contact:<br>Dr." + response.data[0].profile.first_name +
-      " " +
-      response.data[0].profile.last_name +
-      ":</h3><br>" +
-      response.data[0].profile.bio);
+      for (i = 0; i <= 2; i++) {
+        $('.doctor-results').append("We reccomend you contact:<hr><h4>Dr." +
+        response.data[i].profile.first_name +
+        " " +
+        response.data[i].profile.last_name +
+        ":</h4><br>" +
+        response.data[i].profile.bio);
+      }
     });
   });
 });
